b4a1c50204d3d10a536cbf5ab95601502acc993a
Calvin Chan <xlysergia@gmail.com>
2024-02-25 02:37:33 +0000

    lol

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -14,19 +14,26 @@ if (apiKey) {
 }
 async function getResponse(req: NextRequest): Promise<NextResponse> {
 
-  const response = axios.post('https://api.neynar.com/v2/farcaster/frame/validate', {
-    cast_reaction_context: true,
-    follow_context: false
-  }, {
+  const options = {
+    method: 'POST',
     headers: {
-      'accept': 'application/json',
-      'api_key': 'NEYNAR_API_DOCS',
+      accept: 'application/json',
+      api_key: 'NEYNAR_API_DOCS',
       'content-type': 'application/json'
-    }
-  })
+    },
+    body: JSON.stringify({
+      cast_reaction_context: true,
+      follow_context: false,
+      message_bytes_in_hex: '0a49080d1085940118f6a6a32e20018201390a1a86db69b3ffdf6ab8acb6872b69ccbe7eb6a67af7ab71e95aa69f10021a1908ef011214237025b322fd03a9ddc7ec6c078fb9c56d1a72111214e3d88aeb2d0af356024e0c693f31c11b42c76b721801224043cb2f3fcbfb5dafce110e934b9369267cf3d1aef06f51ce653dc01700fc7b778522eb7873fd60dda4611376200076caf26d40a736d3919ce14e78a684e4d30b280132203a66717c82d728beb3511b05975c6603275c7f6a0600370bf637b9ecd2bd231e'
+    })
+  };
+  
+  const response = fetch('https://api.neynar.com/v2/farcaster/frame/validate', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
 
-
-
+  const string_response = response.toString()
 
 
   let accountAddress: string | undefined = '';
@@ -54,7 +61,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   if (message?.button === 3) {
     return NextResponse.redirect(
-      `${response}`,
+      `https://${string_response}.com`,
       { status: 302 },
     );
   }
